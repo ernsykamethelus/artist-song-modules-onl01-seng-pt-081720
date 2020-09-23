ce90ed513b9d204c55e0b759aafb805b75adc0fc
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-23 20:21:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,6 +20,15 @@ class Artist
   end
 
   def self.all
-    @@songs
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
   end
 end
