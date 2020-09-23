e86f04bfd7099bd39e912f1f2e2de63d1529244a
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-23 19:53:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,9 @@ require_relative '../lib/concerns/findable'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  extend Findable 
   include Paramable
+  
   attr_accessor :name
   attr_reader :songs
 
